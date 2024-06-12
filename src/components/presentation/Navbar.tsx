ac6b208d1ae1bbdc05a0ccf1803f493e483b62c0
abd-wanclouds1 <abdullah.rasheed@wanclouds.net>
2024-06-12 23:22:32 +0500

    add temporary navbar logo

--- src/components/presentation/Navbar.tsx
+++ src/components/presentation/Navbar.tsx
@@ -30,11 +30,14 @@ const Navbar = () => {
       <header className="w-full" style={{ zIndex: 9 }}>
         <div className="mx-auto homepage-container homepage-container-resp text-white px-[20px] md:px-[50px] lg:px-[80px] xl:px-[90px] 2xl:px-[200px] overflow-hidden">
           <a href="/">
-            <img
+            {/* <img
               src={logo}
               alt="logo"
               className="w-[120px] h-[50px] lg:w-[150px] lg:h-[70px] 2xl:w-[200px] 2xl:h-[82px]"
-            />
+            /> */}
+            <p className="uppercase font-futura-bold font-bold text-white text-[16px] md:text-[18px] lg:text-[20px] xl:text-[22px] 2xl:text-[24px]">
+              LOGO
+            </p>
           </a>
         </div>
       </header>
